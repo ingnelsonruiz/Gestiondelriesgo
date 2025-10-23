1c841cea5af320c1a13e1916efe240f7d4a90655
Nelson Ruiz <profesionaldesistemas1@dusakawiepsi.com>
2025-10-23 16:27:41 +0000

    Try fixing this error: `Runtime Error: Error: React.Children.only expect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,11 +69,9 @@ export default function RootLayout({
             </Sidebar>
             <SidebarInset>
                 <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                    <SidebarTrigger className="md:hidden" asChild>
-                        <Button size="icon" variant="outline">
-                            <PanelLeft className="h-5 w-5" />
-                            <span className="sr-only">Abrir/Cerrar Menú</span>
-                        </Button>
+                    <SidebarTrigger className="md:hidden">
+                        <PanelLeft className="h-5 w-5" />
+                        <span className="sr-only">Abrir/Cerrar Menú</span>
                     </SidebarTrigger>
                 </header>
                 {children}
